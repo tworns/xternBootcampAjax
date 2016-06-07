38b9e39bc584813aa0e1693a6713d617ca6f3eb7
tworns <tworns@yahoo.com>
2016-06-06 21:41:42 -0400

    Initial commit, based from json-person and mutant add functionality working

---
+++ js/scripts.js
@@ -0,0 +1,118 @@
+var people = [];
+
+$('#menuToggle').on('click', function() {
+  $('nav ul').toggle(400);
+});
+
+$('a[data-remote="true"]').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href') + '?callback=loadResults',
+    method: 'get',
+    dataType: 'jsonp'
+  });
+});
+
+$('a[data-remote-mutants="true"]').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'get',
+    success: loadMutants
+  });
+});
+
+$('a[data-remote-pokemon="true"]').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'get',
+    success: getAllPokemon
+  });
+});
+
+$('a[data-add-mutants = "true"]').on('click',function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'post',
+    headers: {
+        "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      real_name: $('#realName').val(),
+      mutant_name: $('#mutantName').val(),
+      power: $('#mutantPower').val(),
+    }),
+    success: addedMutant(),
+      });
+});
+
+function addedMutant() {
+  $('.people').empty();
+  $('#realName').val("");
+  $('#mutantName').val("");
+  $('#mutantPower').val("");
+  loadMutants();
+}
+
+function loadResults(data) {
+  if (data.firstName) {
+    people.push(data);
+  }
+  else if (data.people) {
+    people = people.concat(data.people);
+  }
+  listPeople();
+}
+
+function loadMutants(data) {
+  $.each(data, function(i, mutant) {
+    people.push({
+      firstName: mutant.mutant_name,
+      lastName: '[' + mutant.real_name + ']',
+      secret: mutant.power
+    });
+  });
+  listPeople();
+}
+
+function getAllPokemon(data) {
+  $.each(data.results, function(i, pokemon) {
+    $.ajax({
+      url: pokemon.url,
+      method: 'get',
+      success: loadPokemon
+    });
+  });
+  setTimeout(function() {
+    listPeople();
+  }, 5000);
+}
+
+function loadPokemon(pokemon) {
+  people.push({
+    firstName: pokemon.name,
+    lastName: '',
+    secret: pokemon.abilities[0].ability.name
+  });
+}
+
+function listPeople() {
+  $('#people').slideUp();
+  $('#people').empty();
+  $.each(people, function(index, person) {
+    var item = $('#template').clone().attr('id', '');
+    item.html(item.html().replace('{{ person.firstName }}', person.firstName)
+      .replace('{{ person.lastName }}', person.lastName)
+      .replace('{{ person.secret }}', person.secret))
+      .removeClass('hide');
+    $('#people').append(item);
+  });
+
+  $('#people').slideDown();
+}
+
+
+
+listPeople();
